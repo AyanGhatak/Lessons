ae0ad0de7ca1a674dbf5ad19004f65e84b03a757
Ayan Ghatak <ayan.ghatak@fusioncharts.com>
2015-07-16 10:48:18 +0530

    10:48am

---
+++ IntroductionToHTML/javascript/myscript.js
@@ -0,0 +1,154 @@
+var tabLinks = new Array();
+var contentDivs = new Array();
+
+function init() {
+
+  // Grab the tab links and content divs from the page
+  var tabListItems = document.getElementById('tabs').childNodes;
+
+  for ( var i = 0; i < tabListItems.length; i++ ) {
+    
+    if ( tabListItems[i].nodeName == "LI" ) {
+
+      var tabLink = getFirstChildWithTagName( tabListItems[i], 'A' );
+      var id = getHash( tabLink.getAttribute('href') );
+      
+      tabLinks[id] = tabLink;
+      contentDivs[id] = document.getElementById( id );
+    }
+  }
+
+  // Assign onclick events to the tab links, and
+  // highlight the first tab
+  var i = 0;
+
+  for ( var id in tabLinks ) {
+    tabLinks[id].onclick = showTab;
+    tabLinks[id].onfocus = function() { this.blur() };
+    if ( i == 0 ) tabLinks[id].className = 'selected';
+    i++;
+  }
+
+  // Hide all content divs except the first
+  var i = 0;
+
+  for ( var id in contentDivs ) {
+    if ( i != 0 ) contentDivs[id].className = 'tabContent hide';
+    i++;
+  }
+}
+
+function showTab() {
+  var selectedId = getHash( this.getAttribute('href') );
+
+  // Highlight the selected tab, and dim all others.
+  // Also show the selected content div, and hide all others.
+  for ( var id in contentDivs ) {
+    if ( id == selectedId ) {
+      tabLinks[id].className = 'selected';
+      contentDivs[id].className = 'tabContent';
+    } else {
+      tabLinks[id].className = '';
+      contentDivs[id].className = 'tabContent hide';
+    }
+  }
+
+  // Stop the browser following the link
+  return false;
+}
+
+function getFirstChildWithTagName( element, tagName ) {
+  for ( var i = 0; i < element.childNodes.length; i++ ) {
+    
+    if ( element.childNodes[i].nodeName == tagName ) return element.childNodes[i];
+  }
+}
+
+function getHash( url ) {
+  var hashPos = url.lastIndexOf ( '#' );
+  return url.substring( hashPos + 1 );
+}
+
+function checkEmail(email){ 
+  
+  var pattern=/^([a-zA-Z0-9_.-])+@([a-zA-Z0-9_.-])+\.([a-zA-Z])+([a-zA-Z])+/;
+  
+  if(pattern.test(email)) {         
+  return true;
+  } else {   
+  return false; 
+  }
+}
+
+function processFormData() { 
+  var name_element = document.getElementById('txt_name');
+  var country_element = document.getElementById('txt_country');
+  var email_element = document.getElementById('txt_email');
+  var add_element = document.getElementById('txt_add');
+  
+  var name = name_element.value.trim();
+  var country = country_element.value.trim();
+  var email = email_element.value.trim();
+  var add = add_element.value,
+      sex, obj=new Object(), dataArr=new Array();
+
+  var error_message = 'The following fields had errors in them: \n\n';
+  var data = 'You entered the following information: \n\n';
+
+  var error_flag = false;
+  
+  if(name == '') {
+    error_message += 'Name: Please enter your name\n';
+    error_flag = true;
+  } else {
+    obj.name = name;
+    data += 'Name: ' + name + '\n';
+  }
+
+  if(country == '') {
+    error_message += 'Country: Please enter your country\n';
+    error_flag = true;
+  } else {
+    obj.country=country;
+    data += 'Country: ' + country + '\n';
+  }
+
+  if(!checkEmail(email)) {
+    error_message += 'Email: Please enter a valid email address';
+    error_flag = true;
+  } else {
+    obj.email=email;
+    data += 'Email: ' + email + '\n';
+  }
+  obj.add=add;
+  data += 'Address: ' + add.trim()+ '\n';
+
+  if (document.getElementById('male').checked) {
+    sex = document.getElementById('male').value;
+  }
+  if (document.getElementById('female').checked) {
+    sex = document.getElementById('female').value;
+  }
+  obj.sex=sex;
+  data += 'Sex: '+sex + "\n";
+  obj.arr = new Array();
+
+  data += 'Interest: ';
+  if (document.getElementById('football').checked) {
+    data += document.getElementById('football').value+ " ";
+    obj.arr.push(document.getElementById('football').value);
+  }
+  if (document.getElementById('movie').checked) {
+    data += document.getElementById('movie').value+ " ";
+    obj.arr.push(document.getElementById('football').value);
+  }
+  if (document.getElementById('reading').checked) {
+    data += document.getElementById('reading').value+ " ";
+    obj.arr.push(document.getElementById('reading').value);
+  }
+  if(error_flag) {
+    console.log(error_message);
+  } else {
+    console.log(data);
+  }
+}
